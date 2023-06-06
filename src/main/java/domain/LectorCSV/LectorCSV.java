f6d44e4eca455d828d6ce4620806b7661daec2fe
lucasDeferrari <ldeferrari@frba.utn.edu.ar>
2023-06-06 14:57:12 -0300

    Changes by Agus

--- src/main/java/domain/LectorCSV/LectorCSV.java
+++ src/main/java/domain/LectorCSV/LectorCSV.java
@@ -5,6 +5,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import retrofit2.http.Path;
 
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -37,3 +38,49 @@ public class LectorCSV {
     }
 
 }
+/*
+import java.io.File; // este CHATGPT
+
+
+    public static void csvUploader(File archivoCSV) {   // ruta de archivo variable.
+        try {
+            Reader reader = Files.newBufferedReader(archivoCSV.toPath());
+
+            CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+            for (CSVRecord registro : parser) {
+                String id = registro.get(0);
+                String nombre = registro.get(1);
+                String contacto = registro.get(2);
+
+                // Resto del código
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static final String Ruta_Archivo = variable de la interfaz que me devuelve el path. Problema de despues.
+
+    public static void csvUploader () {// recibe el csv y lo lee.
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(Ruta_Archivo));
+
+            CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+            for (CSVRecord registro : parser) {
+                String id = registro.get(0);
+                String nombre = registro.get(1);
+                String contacto = registro.get(2);
+
+                //para printear
+//                System.out.println("ID " + id );
+//                System.out.println("nombre " + nombre );
+//                System.out.println("contacto " + contacto );
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+/*
